50bc991799b5b61d61aee1ca05f154692604d6af
thalusantos@hotmail.com <thalusantos@hotmail.com>
2023-06-20 20:10:10 -0300

    Change2

--- database/migrations/2023_05_22_055833_create_users_table.php
+++ database/migrations/2023_05_22_055833_create_users_table.php
@@ -2,13 +2,12 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
@@ -17,15 +16,21 @@ return new class extends Migration
             $table->string('email', 64)->unique();
             $table->string('password', 64);
             $table->string('user_image')->nullable();
-            $table->rememberToken();
             $table->integer('role')->default(0);
             $table->timestamps();
         });
+
+        DB::table('users')->insert([
+            'name' => 'Administrador',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin123'),
+            'user_image' => null,
+            'role' => '1',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
